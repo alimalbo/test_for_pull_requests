89064dd5140576c1f0aec7b67915a7c9f36b4f0a
alimalbo <112868786+alimalbo@users.noreply.github.com>
2022-09-05 14:27:18 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+def func_1(a,b,c):
+    k = a + b + c
+    print(k)
+
+    
+    
+#comment
+def func_2(a,b,c):
+    print(b)
+
+
+def func_3(a,b,c):
+    print(c)
+
+
+def func_4(a,b,c):
+    print()
